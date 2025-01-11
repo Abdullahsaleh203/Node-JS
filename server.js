4b6a4a9e5a8aac6c53b4d051aee109525a527c11
abdallah gamal <abdallah.saleh203@yahoo.com>
2025-01-11 14:01:38 +0200

    Add directory and file reading functionality in server.js

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 console.log(__dirname);
+
+// read directory
 fs.readdir(__dirname, (err, files) => { 
     files.forEach(file => { 
         console.log(file); 
     });
+});
+// read file
+fs.readFile(__dirname + '/server.js', 'utf8', (err, data) => { 
+    console.log(data); 
 });
 // const filename = ''
 // fs.readFile()
