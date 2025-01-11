8ec09c79bbf9fe0da5971c9d73901fbe18b1ba3e
abdallah gamal <abdallah.saleh203@yahoo.com>
2025-01-11 19:50:25 +0200

    Add CORS middleware and configure Express server in server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 
 PORT = process.env.PORT || 3000;
 const app = express();
-     
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
