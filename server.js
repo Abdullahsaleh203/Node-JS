f2e30d89c5cf2fcaf50b4cf662e9c86d0387c114
abdallah gamal <abdallah.saleh203@yahoo.com>
2025-01-11 19:58:22 +0200

    Enhance server.js by adding middleware for urlencoded payloads and improving comments for clarity

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 import express from "express";
 import cors from "cors";
 
+// `process.env.PORT` is used to get the port number from the environment
 const PORT = process.env.PORT || 3000;
+// Create an express app
 const app = express();
-
+// Middleware
 app.use(express.json());
+// Middleware to parse incoming requests with urlencoded payloads
 app.use(express.urlencoded({ extended: false }));
+// Middleware to enable CORS
 app.use(cors());
-
+// Routes
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
