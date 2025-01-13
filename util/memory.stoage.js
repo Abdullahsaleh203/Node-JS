2e6842fe13a8db8628ede0ced9bfcedf18ee661e
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2025-01-13 12:01:16 +0200

    Add memory storage utility with functions to retrieve keys and values

---
+++ util/memory.stoage.js
@@ -0,0 +1,27 @@
+import MemoryStorage from 'memorystorage';
+
+// here, the MemoryStorage function is available
+let myStorage = new MemoryStorage('my-app');
+
+
+
+export const getkey = (myStorage) => {
+    let keys = [];
+    for (let i=0; i<myStorage.length; i++) {
+        let key = myStorage.key(i);
+        keys.push(key);
+    }
+    return keys;
+}
+export const getValues = (myStorage) => {
+    let values  = [];
+    for (let i=0; i<myStorage.length; i++) {
+        let key = myStorage.key(i);
+        let value = myStorage.getItem(key);
+        values.push(value);
+    }
+    return values;
+}
+
+
+export default myStorage;
